616c61838b4b477a815ecf24600ccda86c470354
Fitiavana <fitiavana.tody@gmail.com>
2024-09-24 11:25:04 +0200

    features:LandingNave with border bottom

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@
 import LandingNav from "@/components/common/navbars/LandingNav";
 import Hero from "@/components/sections/landing/Hero";
 import React from "react";
-
 const Landing: React.FC = (): JSX.Element => {
   return (
-    <section className="landing-hero">
-      <LandingNav />
-      <Hero/>
-    </section>
+    <>
+      <section className="landing-hero">
+        <LandingNav />
+        <Hero />
+      </section>
+      <section></section>
+    </>
   );
 };
 
