ee9a1d16433da77c8d2e1fecbe8deaa304c64225
KiritoEM <johankirito64@gmail.com>
2024-10-09 11:55:47 +0300

    features: add skeleton architecture to farmer/dashboard

--- app/(dashboard)/farmer/dashboard/page.tsx
+++ app/(dashboard)/farmer/dashboard/page.tsx
@@ -8,7 +8,7 @@ const FarmerDashboard: FC = (): JSX.Element => {
     <section className="farmer-dashboard bg-[#F3F9FA] h-screen overflow-x-hidden">
       <FarmerLayout>
         {/* Farm Dashboard Content */}
-        <div className="farm-dashboard__content ml-[330px] h-[450px] px-7 mt-8">
+        <div className="farm-dashboard__content ml-[330px] h-[450px] px-5 mt-8">
           {/* Stats section */}
           <div className="dashboard-stats w-full bg-white h-[200px]"></div>
         </div>
