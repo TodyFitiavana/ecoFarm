56825f057d58f153afde8652bb3b12d546e4b587
KiritoEM <johankirito64@gmail.com>
2024-10-17 12:51:41 +0300

    features : add sections-component -> banner to farmer/assistance

--- app/(dashboard)/farmer/assistance/page.tsx
+++ app/(dashboard)/farmer/assistance/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AssistanceLeftPart from "@/components/sections/farmer/assistance/AssistanceLeftPart";
 import FarmerLayout from "@/layouts/FarmerLayout";
 import React, { FC } from "react";
 
@@ -9,22 +10,8 @@ const FarmerDashboard: FC = (): JSX.Element => {
       <FarmerLayout>
         {/* Farm Dashboard Content */}
         <div className="farm-assistance__content ml-[330px] h-[450px] px-5 mt-8 flex gap-7">
-          {/* Stats section */}
-          <div className="content-left w-[70%]">
-            <div className="banner bg-grey w-full rounded-xl p-6 flex items-center gap-3">
-              <div className="texts flex flex-col gap-3">
-                <h1 className="text-black font-cal-sans text-2xl">
-                  Bienvenue dans l’espace assistance
-                </h1>
-                <p className="text-secondary-foreground">
-                  Sélectionnez une catégorie d'assistance parmi celles proposées
-                  ci-dessous, ajoutez vos produits agricoles et réalisez une
-                  prédiction pour une meilleure récolte.
-                </p>
-              </div>
-              <img src="/bots.svg" alt="bot-icon" className="w-[130px]" />
-            </div>
-          </div>
+          {/* Left content section */}
+          <AssistanceLeftPart />
           <div className="content-right w-[30%]"></div>
         </div>
       </FarmerLayout>
