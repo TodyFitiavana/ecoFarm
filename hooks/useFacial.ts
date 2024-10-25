b6529f07808ea7b6c0189e67053c3eb0a206fcdb
KiritoEM <johankirito64@gmail.com>
2024-10-25 17:28:49 +0300

    feat: update useFacial hook add face-detection

--- hooks/useFacial.ts
+++ hooks/useFacial.ts
@@ -1,15 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import * as faceapi from "face-api.js";
-import { loadModels } from "@/lib/facial";
+import { detectFace, loadModels } from "@/lib/facial";
 
 const useFacial = () => {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDetected, setIsDetected] = useState<boolean>(false);
 
   useEffect(() => {
     loadModels();
   }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      detectFace(webcamRef, canvasRef, setIsDetected);
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (isDetected) {
+      console.log("face detected");
+    }
+  }, [isDetected]);
 
   return { webcamRef, canvasRef };
 };
