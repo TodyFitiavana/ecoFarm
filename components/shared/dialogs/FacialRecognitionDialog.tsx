00aec3e25917862829369173babcf4881b453155
Fitiavana <fitiavana.tody@gmail.com>
2024-10-30 08:51:51 +0100

    features: update some stuffs

--- components/shared/dialogs/FacialRecognitionDialog.tsx
+++ components/shared/dialogs/FacialRecognitionDialog.tsx
@@ -1,13 +1,14 @@
 import {
+  Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
-import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
+import useFacial from "@/hooks/useFacial";
 import dynamic from "next/dynamic";
-import { FC, ReactNode, useState } from "react";
+import Image from "next/image";
+import { FC, Fragment, ReactNode, useState } from "react";
 
 interface FacialDialogProps {
   Trigger: ReactNode;
@@ -21,18 +22,36 @@ const FacialRecognitionDialog: FC<FacialDialogProps> = ({
     { ssr: false }
   );
   const [isOpen, setIsOpen] = useState(false);
+  const { isReady } = useFacial();
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>{Trigger}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <WebCamDynamic />
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="sm:max-w-[445px] h-[470px] p-0">
+        {isReady ? (
+          <Fragment>
+            <div className="p-7 pt-8 absolute w-full h-full">
+              <WebCamDynamic />
+            </div>
+            <DialogHeader className=" p-6 h-full flex items-center justify-end">
+              <DialogTitle className="relative z-30">
+                <span className="text-white text-shadow flex gap-3 items-center pb-4">
+                  <Image
+                    src="/icons/facial-recognition_white.svg"
+                    alt="facial-icons"
+                    width={21}
+                    height={21}
+                  />
+                  Montrez votre visage
+                </span>
+              </DialogTitle>
+            </DialogHeader>
+          </Fragment>
+        ) : (
+          <div className="p-6 absolute w-full h-full bg-[#000000da] rounded-lg flex items-center justify-center">
+            <h3 className="text-white">Chargement de la caméra...</h3>
+          </div>
+        )}
       </DialogContent>
     </Dialog>
   );
