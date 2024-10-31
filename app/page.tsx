3051a6e2b43d9b5dddbe61627ca3d75434d39e5d
KiritoEM <johankirito64@gmail.com>
2024-10-31 11:12:29 +0300

    feat: add Lazy Load for landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,61 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { Providers } from "@/lib/redux/provider";
 import dynamic from "next/dynamic";
 
 const DynamicLandingNav = dynamic(
   () => import("@/components/shared/navbars/LandingNav"),
-  {  loading: () => <LoadingScreen /> }
+  { suspense: true, loading: () => <LoadingScreen /> }
 );
 const DynamicHero = dynamic(
   () => import("@/components/sections/landing/Hero"),
-  {  loading: () => <LoadingScreen /> }
+  { suspense: true, loading: () => <LoadingScreen /> }
 );
 const DynamicAbout = dynamic(
   () => import("@/components/sections/landing/About"),
-  {  loading: () => <LoadingScreen /> }
+  { suspense: true, loading: () => <LoadingScreen /> }
 );
 const DynamicFooter = dynamic(
   () => import("@/components/sections/landing/Footer"),
-  {  loading: () => <LoadingScreen /> }
+  { suspense: true, loading: () => <LoadingScreen /> }
 );
 
 const LoadingScreen = () => (
-  <div className="w-screen h-screen bg-red-300 fixed z-50 flex items-center justify-center">
+  <div className="w-screen h-screen bg-red-200 fixed z-50 flex items-center justify-center">
     loading...
   </div>
 );
 
 const Landing: React.FC = (): JSX.Element => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Providers>
-      <section className="landing-hero">
-        <DynamicLandingNav />
-        <DynamicHero />
-      </section>
-      <section className="landing-About">
-        <DynamicAbout />
-      </section>
-      <section>
-        <DynamicFooter />
-      </section>
+      <Suspense fallback={<LoadingScreen />}>
+        <section className="landing-hero">
+          <DynamicLandingNav />
+          <DynamicHero />
+        </section>
+        <section className="landing-About">
+          <DynamicAbout />
+        </section>
+        <section>
+          <DynamicFooter />
+        </section>
+      </Suspense>
     </Providers>
   );
 };
